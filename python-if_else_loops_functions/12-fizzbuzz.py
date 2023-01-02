c9c16921e974538044e5d0417c1347daa982760d
root <harishch@brightcomgroup.com>
2023-01-02 04:52:03 -0800

    12-fizzbuzz.py

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(1,100):
-        if i % 3 == 0 and i % 5 == 0:
+    for num in range(1, 101):
+        if num % 15 == 0 and num % 3 == 0:
             print("FizzBuzz", end=" ")
-        elif i % 5 == 0:
+        elif num % 3 == 0:
+            print("Fizz", end=" ")
+        elif num % 5 == 0:
             print("Buzz", end=" ")
-            continue
-        elif i % 3 == 0:
-            print("Fuzz", end=" ")
-        print(i, end=" ")
-fizzbuzz()
+        else:
+            print(num, end=" ")
