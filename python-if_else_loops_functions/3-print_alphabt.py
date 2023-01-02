1155606f59a0ae58d91543bf61594b7ede720190
root <harishch@brightcomgroup.com>
2023-01-01 22:40:29 -0800

    3-print_alphabt.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 for i in range(97, 123):
-    if i == 101:
+    if i == 101 :
         continue
-    if i == 113:
+    if i == 113 :
         continue
+
     print(chr(i), end="" .format(chr))
