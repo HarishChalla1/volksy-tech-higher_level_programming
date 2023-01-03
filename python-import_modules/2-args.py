ff7b4b0b30e2bbdaf2d70dd30ee3d6734c56729e
root <harishch@brightcomgroup.com>
2023-01-03 07:59:12 -0800

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,11 @@
-#!/usr/bin/python3
-if _name_ == "_main_":
-    import sys
-    length = len(sys.argv)
-    if length <= 1:
-        print("0 arguments.")
-    elif length == 2:
-        print("{:d} argument:".format(length - 1))
-    else:
-        print("{:d} arguments:".format(length - 1))
-    for i in range(1, length):
-        print("{:d}: {:s}".format(i, sys.argv[i]))
+#!/usr/bin/pythpon3
+import sys
+length = len(sys.argv)
+if length <= 1:
+    print("0 arguments.")
+elif length == 2:
+    print("{:d} argument:".format(length - 1))
+else:
+    print("{:d} arguments:".format(length - 1))
+for i in range(1, length):
+    print("{:d}: {:s}".format(i, sys.argv[i]))
