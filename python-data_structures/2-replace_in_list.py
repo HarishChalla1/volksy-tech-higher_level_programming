598b2f0366ceb61a1f5e4a6ecc8a36f42502fe48
root <harishch@brightcomgroup.com>
2023-01-05 21:40:00 -0800

    1-element_at.py

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -4,3 +4,9 @@ def replace_in_list(my_list, idx, element):
         return my_list
     if idx > len(my_list):
         return my_list
+    for i in range(len(my_list)):
+        new_list = replace_in_list(my_list,idx,new_element)
+        if my_list[i] == new_element:
+            print(new_list)
+        else:
+            print(element)
