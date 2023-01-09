53df66969ddbbbf5edec57624e724f9254c45ff5
root <harishch@brightcomgroup.com>
2023-01-08 22:46:37 -0800

    12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,45 +1,22 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    # Fail checks, none, not a string
-    if not roman_string:
-        return 0
-    if not isinstance(roman_string, str):
-        return 0
-    if not roman_string.isupper():
-        return 0
-    # Dictionary for roman numerals
-    r_dict = {
-        "I": 1,
-        "IV": 4,
-        "V": 5,
-        "IX": 9,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000
-    }
-
-    result = 0
-    temp = list(roman_string)
-    # Concat 4 and 9s
-    if len(temp) > 1:
-        idx = 0
-        for i in temp:
-            try:
-                if temp[idx] == 'I' and temp[idx + 1] == 'V':
-                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
-            except IndexError:
-                pass
-            try:
-                if temp[idx] == 'I' and temp[idx + 1] == 'X':
-                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
-            except IndexError:
-                pass
-            idx += 1
-    # Search in dict for correct numbers and add
-    for k, v in r_dict.items():
-        for index in temp:
-            if index == k:
-                result += v
-    return result
+    if not(roman_string):
+        return(0)
+    if type(roman_string) != str:
+        return(0)
+    value = 0
+    convert = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    for i in range(len(roman_string)):
+        for x, y in convert.items():
+            if i != len(roman_string) - 1:
+                if roman_string[i] == x:
+                    for x2, y2 in convert.items():
+                        if roman_string[i+1] == x2:
+                            if y2 > y:
+                                value -= y
+                            else:
+                                value += y
+            else:
+                if roman_string[i] == x:
+                    value += y
+    return(value)
