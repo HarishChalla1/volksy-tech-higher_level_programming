98e032e07d122709f0965b7fd631f1456286f0aa
root <harishch@brightcomgroup.com>
2023-01-12 00:41:31 -0800

    3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """square size"""
-    def __init__(self, size=0):
+    def __init__(self, size=0):
         """constructor"""
         if type(size) is not int:
             raise TypeError("size must be an integer")
@@ -12,7 +12,7 @@ class Square:
             raise ValueError("size must be >= 0")
         self .__size = size
 
-    def area(self):
+    def area(self):
         """area of square"""
-        return (self.__size * self.__size)
+        return self.__size * self.__size
 
