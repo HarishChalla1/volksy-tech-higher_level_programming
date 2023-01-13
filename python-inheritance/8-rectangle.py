9230c41d285b6cee28aa17cd2ffeb7f7da9fb4da
root <harishch@brightcomgroup.com>
2023-01-13 01:16:23 -0800

    5-base_geometry.py

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -4,21 +4,14 @@
 
 
 class BaseGeometry:
-    """Contains functions area(), integer_validator()
-    """
-
+    """Contains functions area(), integer_validator()"""
     def area(self):
-        """Function not implemented yet
-        """
-
+        """Function not implemented yet"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Function that validates `value`
-        """
-
+        """Function that validates `value`"""
         if type(value) != int:
             raise TypeError(name + " must be an integer")
-
         if value <= 0:
             raise ValueError(name + " must be greater than 0")
