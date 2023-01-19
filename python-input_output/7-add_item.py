7e68e6aa972cf762eb9fefe118863c981f14a79e
root <harishch@brightcomgroup.com>
2023-01-18 22:21:41 -0800

    8-class_to_json.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,17 +3,17 @@
 import sys
 
 
-save_to_json_file = _import_('5-save_to_json_file').save_to_json_file
-load_from_json_file = _import_('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 fname = "add_item.json"
-List = []
+ListyBoii = []
 
 try:
-    List = load_from_json_file(fname)
+    ListyBoii = load_from_json_file(fname)
 except:
     pass
 
 for i in range(1, len(sys.argv)):
-    List.append(sys.argv[i])
-save_to_json_file(List, fname)
+    ListyBoii.append(sys.argv[i])
+save_to_json_file(ListyBoii, fname)
