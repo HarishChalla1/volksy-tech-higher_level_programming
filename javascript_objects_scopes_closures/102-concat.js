a8415fa027e83689906da4778146bee3b164618f
root <harishch@brightcomgroup.com>
2023-02-25 19:59:56 -0800

    tsk102

--- javascript_objects_scopes_closures/102-concat.js
+++ javascript_objects_scopes_closures/102-concat.js
@@ -1,7 +1,22 @@
 #!/usr/bin/node
-// JS Script
+
+let finalData = '';
 const fs = require('fs');
-const text1 = fs.readFileSync(process.argv[2], 'utf-8');
-const text2 = fs.readFileSync(process.argv[3], 'utf-8');
-fs.appendFile(process.argv[4], text1.concat(text2).trim());
-fs.appendFile(process.argv[4], '\n');
+
+fs.readFile(process.argv[2], (err, data) => {
+  if (err) throw err;
+
+  const dataFile1 = data.toString();
+  finalData += dataFile1;
+});
+
+fs.readFile(process.argv[3], (err, data) => {
+  if (err) throw err;
+
+  const dataFile2 = data.toString();
+  finalData += dataFile2;
+  const fs2 = require('fs');
+  fs2.writeFile(process.argv[4], finalData, (err) => {
+    if (err) throw err;
+  });
+});
